a25706ca9a2d0ce90ac3882c5809dc0cdc14c15f
hugz thomas <hugues.lam.thomas@gmail.com>
2017-02-13 17:58:30 +0100

    changed model furnitures and associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :furniture
+  belongs_to :offer
   belongs_to :user
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,4 @@
-class Furniture < ApplicationRecord
+class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
 end
