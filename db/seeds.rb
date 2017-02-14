0fe182529bda6bdef3e81e05660f7d1a64e4e71d
Quentin Casaubon <quentincasaubonweb@gmail.com>
2017-02-14 10:16:02 +0100

    structure of the home page, search bar as partial V1 and seed for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+10.times do
+  offer = Offer.new(
+    name: Faker::StarWars.character,
+    image_url: Faker::Company.logo,
+    description: Faker::Team.name,
+    price: rand(1...50),
+    address: Faker::Address.street_name,
+    category: Faker::Beer.hop,
+    user_id: 1)
+  offer.save
+end
