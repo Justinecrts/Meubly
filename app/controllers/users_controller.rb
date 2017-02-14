33cdfbd074bcf6fd26f010604e8e14ae24141753
Justine Courtois <justine.crts@gmail.com>
2017-02-14 13:41:20 +0100

    AddedDashbordNavbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
+    @user_offers = Offer.where(user_id: @user)
   end
 
   private
-
   def set_user
     @user = User.find(params[:id])
   end
