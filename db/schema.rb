76334e5b4005e59fb7e808ac1d50e779de1950ca
Quentin Casaubon <quentincasaubonweb@gmail.com>
2017-02-14 17:37:38 +0100

    adding the list for the category in the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170214155050) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
