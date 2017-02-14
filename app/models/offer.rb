fc175b5e5939ac8da83fa9b5fa880a59758c39e4
Quentin Casaubon <quentincasaubonweb@gmail.com>
2017-02-14 11:09:29 +0100

    fixing model offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  validates :name, presence: true
+  validates :category, inclusion: { in: ["meubles", "luminaires", "electromenager", "jardin"], allow_nil: false }
+  CATEGORY = ["meubles", "luminaires", "electromenager", "jardin"]
 end
