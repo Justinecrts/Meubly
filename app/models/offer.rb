6bca668f68bf6ac26b1e8185ba5d85aa33172c88
hugz thomas <hugues.lam.thomas@gmail.com>
2017-02-15 14:52:23 +0100

    geocoder

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,7 @@ class Offer < ApplicationRecord
   validates :category, inclusion: { in: ["meubles", "luminaires", "electromenager", "jardin"], allow_nil: false }
   CATEGORY = ["meubles", "luminaires", "electromenager", "jardin"]
   mount_uploader :photo, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
 end
